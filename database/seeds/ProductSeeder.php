74ac2fb682564a6b6b306935f01519eae4b48a99
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-14 20:09:35 +0530

    feat: configure product seeder to add fake products

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -11,6 +11,8 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(App\Product::class, 100)->create()->each(function ($product) {
+            $product->reviews()->createMany(factory(App\Review::class, 5)->make()->toArray());
+        });
     }
 }
