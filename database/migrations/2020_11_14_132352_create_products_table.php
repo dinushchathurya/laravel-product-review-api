9d041053517b28bde2552b2c9a763d512329922b
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-14 19:03:48 +0530

    refactor: product migration file and defining its relationship with the users table

--- database/migrations/2020_11_14_132352_create_products_table.php
+++ database/migrations/2020_11_14_132352_create_products_table.php
@@ -15,7 +15,16 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->decimal('price');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
