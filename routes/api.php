00e7fa2ec8a35ac6bfe7d2ae85373222b5acda39
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-14 23:13:48 +0530

    fix: typo error in login route in api.php

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('me', 'AuthController@me');
 Route::post('register', 'AuthController@register');
-Routre::post('login', 'AuthController@login');
+Route::post('login', 'AuthController@login');
 Route::post('logout', 'AuthController@logout');
 
 Route::apiResource('products', 'ProductController');
