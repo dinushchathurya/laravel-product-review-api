9c8e866c4b7ad26ef5775ff0b536a0b02320bb0a
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-14 19:46:16 +0530

    feat: define the inverse relationship to the user and product model

--- app/Review.php
+++ app/Review.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    /* Get the product that owns the review */
+    public function product()
+    {
+        return $this->belongsTo('App\Product');
+    }
+
+    /* Get the user that made the review */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
