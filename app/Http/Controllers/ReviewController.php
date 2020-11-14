a39e7a13500094ceb5c4aba8e6b1955d529f5133
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-14 22:35:43 +0530

    feat: finish function implementation for add review, update review & delete review

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Review;
+use App\Product;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
-{
+{   
+    public function __construct()
+    {
+      $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -35,7 +41,18 @@ class ReviewController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'review' => 'required|string',
+            'rating' => 'required|numeric|min:0|max:5',
+        ]);
+
+        $review = new Review;
+        $review->review = $request->review;
+        $review->rating = $request->rating;
+        $review->user_id = auth()->user()->id;
+
+        $product->reviews()->save($review);
+        return response()->json(['message' => 'Review Added', 'review' => $review]);
     }
 
     /**
@@ -69,7 +86,19 @@ class ReviewController extends Controller
      */
     public function update(Request $request, Review $review)
     {
-        //
+        if (auth()->user()->id !== $review->user_id) {
+            return response()->json(['message' => 'Action Forbidden']);
+        }
+        $request->validate([
+            'review' => 'required|string',
+            'rating' => 'required|numeric|min:0|max:5',
+        ]);
+
+        $review->review = $request->review;
+        $review->rating = $request->rating;
+        $review->save();
+
+        return response()->json(['message' => 'Review Updated', 'review' => $review]);
     }
 
     /**
@@ -80,6 +109,10 @@ class ReviewController extends Controller
      */
     public function destroy(Review $review)
     {
-        //
+        if (auth()->user()->id !== $review->user_id) {
+            return response()->json(['message' => 'Action Forbidden']);
+        }
+        $review->delete();
+        return response()->json(null, 204);
     }
 }
