80db5e8d67dc39e28e533119f361ced6d0a22062
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-14 19:42:15 +0530

    feat:  define the relationship between the product and the review model

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /* Get the reviews of the product */
+    public function reviews()
+    {
+        return $this->hasMany('App\Review');
+    }
+
+    /* Get the user added product */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
