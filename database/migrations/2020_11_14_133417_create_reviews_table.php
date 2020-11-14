f90b189fb7730137f3a762a8373008f719b06739
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-14 19:09:51 +0530

    refactor: review migration file and define its relationships with users and products

--- database/migrations/2020_11_14_133417_create_reviews_table.php
+++ database/migrations/2020_11_14_133417_create_reviews_table.php
@@ -15,7 +15,21 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('product_id');
+            $table->text('review');
+            $table->integer('rating');
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
         });
     }
 
