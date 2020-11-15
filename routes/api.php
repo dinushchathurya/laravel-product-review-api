dfbc6b1297f5a04ff575a409798a215d6b2873c8
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-15 10:26:05 +0530

    fix: route prefix error in product resource

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
 Route::get('me', 'AuthController@me');
-Route::post('register', 'AuthController@register');
 Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
 Route::post('logout', 'AuthController@logout');
 
 Route::apiResource('products', 'ProductController');
